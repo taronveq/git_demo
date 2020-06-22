7e3fc74233a308988ba89abbf896849b08c344bb
taronveq <taron.veqilyan@gmail.com>
2020-06-22 10:37:18 +0400

    Added Project root folder

---
+++ git_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_project
+//
+//  Created by MacBook Pro on 6/22/20.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
