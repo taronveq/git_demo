1a967c7b14d65faf953d77916204beb4f665b744
Hovo <malxasyan.18bk.ru@MacBook-Air-Hovo.local>
2020-06-24 11:34:00 +0400

    changes

--- git_project/ViewController.swift
+++ git_project/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("bug fix bchange")
         print("bug fix achange")
-        print("Hovo")
+        print("Malkhasyan")
         print ("123")
         print("gag")
     }
