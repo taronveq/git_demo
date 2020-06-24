28208220353f87548cb71cebb46a2e06e6b17fdf
Davit <davit@Davits-MacBook-Air.local>
2020-06-24 11:15:04 +0400

    11

--- git_project/ViewController.swift
+++ git_project/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("bug fix bchange")
         print("bug fix achange")
+        print("bug fix dchange")
     
         print("gag")
     }
